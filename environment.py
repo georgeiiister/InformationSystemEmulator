5c25862298db509ffae64c5ca9553aeee4309698
georgeiiister <georgeiiister@gmail.com>
2024-09-06 20:10:52 +0300

    add new module for create environment

---
+++ environment.py
@@ -0,0 +1,6 @@
+import setting
+
+class Environment:
+    def __init__(self):
+        self.__initial_settings = setting.InitialSettings()
+        self.__system_setting = setting.SystemSettings()
--- setting.py
+++ setting.py
@@ -1,10 +1,9 @@
-class Tuning:
+class InitialSettings:
     def __init__(self):
         self.__program_name = ('information', 'system', 'emulator')
         self.__directory_name_of_program = 'ISE'
         self.__directory_name_of_db_program = 'DB_ISE'
-        self.__file_name_of_sequence='seq'
-        self.__file_name_of_account='account'
+
 
     @property
     def program_name(self):
@@ -17,6 +16,12 @@ class Tuning:
     @property
     def directory_name_of_db_program(self):
         return self.__directory_name_of_db_program
+
+
+class SystemSettings:
+    def __init__(self):
+        self.__file_name_of_sequence='seq'
+        self.__file_name_of_account='account'
 
     @property
     def file_name_of_sequence(self):
