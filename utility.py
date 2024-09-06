ee7ccfd15b81aeb8e35b000485ad1ac5bc916492
georgeiiister <georgeiiister@gmail.com>
2024-09-06 16:22:47 +0300

    refactoring code of class Collection and CollectionNumber

--- utility.py
+++ utility.py
@@ -8,7 +8,8 @@ class Collection:
         self.__count = 0
         self.__count_item = None
         self.__histogram = dict()
-        self.__min_max = 0
+        self.__collection_tuple = tuple()
+        self.__ucc = dict() #universal collection cache
 
     @property
     def collection_value(self):
@@ -21,7 +22,17 @@ class Collection:
         self.__count = None
         self.__count_item = None
         self.__histogram = dict()
-        self.__min_max = (None,None)
+        self.__collection_tuple = tuple()
+        self.__ucc = dict()
+
+    def exists_key(self, ucc_key):
+        return ucc_key in self.__ucc
+
+    def set_value_in_ucc(self,ucc_key, ucc_value):
+        self.__ucc[ucc_key] = ucc_value
+
+    def get_value_in_ucc(self,ucc_key):
+        return self.__ucc.get(ucc_key)
 
     def count(self, item: Optional = None):
         if item:
@@ -39,32 +50,43 @@ class Collection:
             self.__count = self.__count or len(self.__value)
             return self.__count
 
+    def deep_count(self):
+        raise NotImplemented #DOIT
+
     def __len__(self):
         return self.count()
 
     @property
-    def min_max(self) -> Optional[tuple]:
-        self.__min_max = self.__min_max or min(self.collection_value), max(self.collection_value)
-        return self.__min_max
+    def vector_tuple(self):
+        return self.__collection_tuple or self.__value2vector_tuple()
+
+    def __value2vector_tuple(self,value:Union[list,tuple,str] = None):
+        value = value or self.collection_value
+        for item in value:
+            try:
+                self.__collection_tuple = self.__value2vector_tuple(item)
+            except TypeError:
+                self.__collection_tuple += (item,)
+        return self.__collection_tuple
 
     @property
     def deep_min_max(self):
-        raise NotImplemented  # DOIT
+        key_min = 'min'
+        key_max = 'max'
+        self.set_value_in_ucc(ucc_key = key_min,
+                              ucc_value = self.get_value_in_ucc(key_min) or min(self.vector_tuple)
+                              )
+        self.set_value_in_ucc(ucc_key=key_max,
+                              ucc_value=self.get_value_in_ucc(key_max) or max(self.vector_tuple)
+                              )
+        return self.get_value_in_ucc(ucc_key=key_min), self.get_value_in_ucc(ucc_key=key_max)
 
 class CollectionNumber(Collection):
     def __init__(self,value:Union[list[int],tuple[int]]):
         Collection.__init__(self,value=value)
-        self.__deep_sum = 0
+        self.__deep_sum = None
 
     @property
     def deep_sum(self):
-        return self.__deep_sum or self.__calculate_deep_sum()
-
-    def __calculate_deep_sum(self,value:Optional[int] = None):
-        value = value or self.collection_value
-        for item in value:
-            try:
-                self.__deep_sum = self.__calculate_deep_sum(item)
-            except TypeError:
-                self.__deep_sum += item
+        self.__deep_sum = self.__deep_sum or sum(self.vector_tuple)
         return self.__deep_sum
