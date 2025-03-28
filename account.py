21e7dc4539ccad7877d1d41488502e703a9e7d4d
georgeiiister <georgeiiister@gmail.com>
2025-03-28 16:07:37 +0300

    refactoring

--- account.py
+++ account.py
@@ -87,8 +87,8 @@ class Account:
         self.__describe = describe
         self.__registration_datetime = registration_datetime
         self.__close_datetime = None
-        self.__account_collection = None
-        self.__id_account_in_collection = None
+        self.__collection = None
+        self.__id_in_collection = None
 
         Account.__count += 1
         Account.__internal_id = next(Account.__internal_generator_id)
@@ -97,7 +97,7 @@ class Account:
         if not self.__account_id:
             self.__account_id = Account.__internal_id
 
-        self.__account_number = account_number or f'account_number{self.__account_id}'
+        self.__account_number = account_number or f'number{self.__account_id}'
 
         if activation_datetime is not None:
             self.activation(activation_datetime=activation_datetime)
@@ -108,18 +108,19 @@ class Account:
         self.__lock = False
 
     @property
-    def account_collection(self):
-        return self.__account_collection
+    def collection(self):
+        return self.__collection
 
     @property
-    def id_account_in_collection(self):
-        return self.id_account_in_collection
+    def id_in_collection(self):
+        return self.__id_in_collection
 
-    def account_collection_and_id_account_in_collection(self,
-                                                        value,
-                                                        id_account_in_collection) -> None:
-        self.__account_collection = value
-        self.__id_account_in_collection = id_account_in_collection
+    def set_collection(self,
+                       value,
+                       id_in_collection
+                       ) -> None:
+        self.__collection = value
+        self.__id_in_collection = id_in_collection
 
     def credit(self, amount: int) -> None:
         if self.activation_datetime is None:
@@ -159,7 +160,9 @@ class Account:
     def activation_datetime(self):
         return self.__activation_datetime
 
-    def activation(self, activation_datetime:Optional[datetime.datetime] = None):
+    def activation(self,
+                   activation_datetime:Optional[datetime.datetime] = None
+                   ):
         activation_datetime = activation_datetime or datetime.datetime.now()
 
         if self.registration_datetime > activation_datetime:
@@ -168,7 +171,9 @@ class Account:
         self.__activation_datetime = activation_datetime
         self.__state = Account.__states.get('__active')
 
-    def close(self, close_datetime: Optional[datetime.datetime] = None):
+    def close(self,
+              close_datetime: Optional[datetime.datetime] = None
+              ):
         close_datetime = close_datetime or datetime.datetime.now()
 
         if self.registration_datetime > close_datetime:
@@ -196,7 +201,7 @@ class Account:
         return (f'Account(account_id={self.account_id}'
                 f', balance={self.balance}'
                 f', account_number={self.account_number}'
-                f', account_collection={self.account_collection}'
+                f', account_collection={self.collection}'
                 f', describe={self.describe}'
                 f', registration_datetime={self.registration_datetime}'
                 f', activation_datetime={self.activation_datetime}'
@@ -240,7 +245,9 @@ class Accounts:
         self.__accounts: Dict[int, Account] = dict()
         self.__primary_item_id: Optional[int] = None
 
-        self.add_account(account=account, primary=primary)
+        self.add_account(account=account,
+                         primary=primary
+                         )
 
         self.__internal_id = next(Accounts.__internal_generator_id)
 
@@ -249,8 +256,8 @@ class Accounts:
 
     def add_account(self,
                     account: Account,
-                    primary: bool = False  # add as primary account in collection
-                    ):
+                    primary: bool = False
+                    ): # add as primary account in collection
         """Method for add object account to this account collection"""
 
         self.__accounts_by_id[account.account_id] = account
@@ -262,10 +269,13 @@ class Accounts:
             self.__primary_item_id = item_id
 
         # set on account link to this collection
-        account.account_collection_and_id_account_in_collection(value = self,
-                                                                id_account_in_collection = item_id)
+        account.set_collection(value = self,
+                               id_in_collection = item_id
+                              )
 
-    def find_account_by_id(self, account_id: Account.account_id) -> Account:
+    def account_by_id(self,
+                      account_id: Account.account_id
+                      ) -> Account:
         """Find account by internal id account"""
 
         result: Account = self.__accounts_by_id.get(account_id)
@@ -276,28 +286,30 @@ class Accounts:
     def __del_account_by_id(self,
                             account_id: Account.account_id
                             ):
-        account: Account = self.find_account_by_id(account_id = account_id)
-        self.__del_account_by_item_id(item_id = account.id_account_in_collection)
+        account: Account = self.account_by_id(account_id = account_id)
+        self.__del_account_by_item_id(item_id = account.id_in_collection)
 
-    def __del_account_by_item_id(self, item_id: int):
-        account: Account = self.find_account_by_item_id(item_id = item_id)
+    def __del_account_by_item_id(self,
+                                 item_id: int
+                                 ):
+        account: Account = self.account_by_item_id(item_id = item_id)
 
         if account == self.primary:
             raise DeletePrimaryAccount
 
         del self.__accounts_by_id[account.account_id]
-        account.account_collection_and_id_account_in_collection(value = None,
-                                                                id_account_in_collection = None
-                                                                )
+        account.set_collection(value = None, id_in_collection = None)
 
-    def find_account_by_number(self, account_number: Account.account_number) -> List[Account]:
+    def account_by_number(self,
+                          account_number: Account.account_number
+                          ) -> List[Account]:
         """Find account by account number"""
         result = []
         item_id: Optional[int] = -1
         try:
             while True:
                 item_id = self.__accounts_numbers.index(account_number, item_id + 1)
-                result.append(self.find_account_by_item_id(item_id))
+                result.append(self.account_by_item_id(item_id))
         except ValueError:
             if not result:
                 raise AccountNotFoundError
@@ -310,13 +322,15 @@ class Accounts:
         try:
             while True:
                 item_id = self.__accounts_numbers.index(item, item_id + 1)
-                result.append(self.find_account_by_item_id(item_id))
+                result.append(self.account_by_item_id(item_id))
         except ValueError:
             if not result:
                 raise AccountNotFoundError
         return result
 
-    def find_account_by_item_id(self, item_id: int) -> Account:
+    def account_by_item_id(self,
+                           item_id: int
+                           ) -> Account:
         """Find account by item id in collection"""
 
         result: Account = self.__accounts.get(item_id)  # work with dictionary accounts
@@ -328,12 +342,13 @@ class Accounts:
     def primary(self) -> Optional[Account]:
         """Method return primary account in collection"""
         if self.__primary_item_id:
-            return self.find_account_by_item_id(item_id=self.__primary_item_id)
+            return self.account_by_item_id(item_id=self.__primary_item_id)
 
-    def set_primary(self, item_id: int) -> None:
+    @primary.setter
+    def primary(self, item_id: int) -> None:
         """Method set primary account in collection"""
 
-        result: Account = self.find_account_by_item_id(item_id=item_id)
+        result: Account = self.account_by_item_id(item_id=item_id)
         if not result:
             raise AccountNotFoundError
         self.__primary_item_id = item_id
