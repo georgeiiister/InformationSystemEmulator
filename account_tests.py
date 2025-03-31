0d859d03b18f111769c0f8440431489d4f3b8982
georgeiiister <georgeiiister@gmail.com>
2025-03-31 11:01:05 +0300

    pep8 style

--- account_tests.py
+++ account_tests.py
@@ -4,22 +4,22 @@ import account
 import random
 from decimal import Decimal
 
-#pool of accounts
+# pool of accounts
 acc_pool = tuple((account.Account() for i in range(10)))
 
-#activing of accounts
-a_acc = slice(0,int(len(acc_pool)/2)+1)
+# activing of accounts
+a_acc = slice(0, int(len(acc_pool) / 2) + 1)
 for account in acc_pool[a_acc]:
     account.activation()
 
 for i, account in enumerate(acc_pool, 1):
     if account.active:
-        amount:Decimal = Decimal(str(round(random.randint(1,10_000) * random.random(),2)))
-        account.credit(amount = amount)
+        amount: Decimal = Decimal(str(round(random.randint(1, 10_000) * random.random(), 2)))
+        account.credit(amount=amount)
 
-#pool active of account with not zero balance
+# pool active of account with not zero balance
 acc_pool_balance = (account for account in acc_pool if account.balance > 0)
 
 for account in acc_pool_balance:
-    account.debit(amount = account.balance)
+    account.debit(amount=account.balance)
     account.close()
