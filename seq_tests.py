d3e9f96ad8abc92ffeca234951a390346592e9b6
georgeiiister <georgeiiister@gmail.com>
2025-05-01 23:08:48 +0300

    add new test with threads for seq

--- seq_tests.py
+++ seq_tests.py
@@ -1,11 +1,43 @@
 import seq
+import threading
 
 with seq.Seq() as s1:
     for i in range(5):
-        print(next(s1))
+        print(next(s1),end=' ')
+
+print()
 
 s1 = seq.Seq()
 for i in range(5):
-    print(s1.next_id())
+    print(s1.next_id(), end=' ')
+
+print()
+
+class Threads_(threading.Thread):
+    def __init__(self,name, seq):
+        super().__init__(name = name)
+        self.__seq = seq
+        self.__result = []
+
+    def run(self):
+        while True:
+            try:
+                sum(range(10_000_000))
+                self.__result.append(next(self.__seq))
+            except StopIteration:
+                print(self.name+':'+str(self.result))
+                break
+
+    @property
+    def result(self):
+        return self.__result
+
+s2 = seq.Seq(seq_name='check_thread',begin = 1, step = 1, end=20)
+
+threads = (Threads_(name=f'thread{i}',seq = s2) for i in range(4))
+
+for i in threads:
+    i.start()
+
 
 
